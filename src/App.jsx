fb2a60703cea27e166d7d18965e7738770a87a89
jd richards <jdrichardstech@gmail.com>
2023-12-12 03:20:00 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 const App = () => {
-  return <div>Basic Pursuit React Starter</div>;
+  return (
+    <div className="container">
+      <form>
+        <label htmlFor="firstName">First name:</label>
+        <input type="text" id="firstName" />
+        <label htmlFor="lastName">Last name:</label>
+        <input type="text" id="lastName" />
+        <label htmlFor="zip">Zip code:</label>
+        <input type="number" id="zip" />
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" />
+        <label htmlFor="pets">Pets</label>
+        <select>
+          <option value=""></option>
+          <option value="cats">cats</option>
+          <option value="dogs">dogs</option>
+          <option value="none">none</option>
+        </select>
+        <label htmlFor="admin">Admin</label>
+        <input type="checkbox" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
