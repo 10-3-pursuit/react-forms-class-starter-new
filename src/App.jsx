95ec5ac44c0adb46c22053e2ab5f362e5e719eba
jd richards <jdrichardstech@gmail.com>
2023-12-12 08:29:34 -0500

    change admin

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,38 @@ const App = () => {
   return (
     <div className="container">
       <form>
-        <label htmlFor="firstName">First name:</label>
-        <input type="text" id="firstName" name="firstName" />
-        <label htmlFor="lastName">Last name:</label>
-        <input type="text" id="lastName" name="lastName" />
-        <label htmlFor="zip">Zip code:</label>
-        <input type="text" id="zip" name="zip" />
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" />
-        <label htmlFor="pets">Pets</label>
-        <select id="pets" name="pets">
-          <option value=""></option>
-          <option value="yes">yes</option>
-          <option value="no">no</option>
-        </select>
-        <label htmlFor="admin">Admin</label>
-        <input type="checkbox" id="admin" name="admin" />
+        <label htmlFor="firstName">
+          First name:
+          <input type="text" id="firstName" name="firstName" />
+        </label>
+        <label htmlFor="lastName">
+          Last name:
+          <input type="text" id="lastName" name="lastName" />
+        </label>
+        <label htmlFor="zip">
+          Zip code:
+          <input type="text" id="zip" name="zip" />
+        </label>
+        <label htmlFor="email">
+          Email:
+          <input type="email" id="email" name="email" />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input type="password" id="password" name="password" />
+        </label>
+        <label htmlFor="pets">
+          Pets
+          <select id="pets" name="pets">
+            <option value=""></option>
+            <option value="yes">yes</option>
+            <option value="no">no</option>
+          </select>
+        </label>
+        <label htmlFor="dev">
+          Developer
+          <input type="checkbox" id="dev" name="dev" />
+        </label>
         <button>Submit</button>
       </form>
     </div>
