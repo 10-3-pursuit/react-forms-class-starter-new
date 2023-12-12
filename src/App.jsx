68ae748ffc48c5da3b6ba39bb790a4ef4e532a39
jd richards <jdrichardstech@gmail.com>
2023-12-12 04:19:38 -0500

    add names

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import React from "react";
-
 const App = () => {
   return (
     <div className="container">
       <form>
         <label htmlFor="firstName">First name:</label>
-        <input type="text" id="firstName" />
+        <input type="text" id="firstName" name="firstName" />
         <label htmlFor="lastName">Last name:</label>
-        <input type="text" id="lastName" />
+        <input type="text" id="lastName" name="lastName" />
         <label htmlFor="zip">Zip code:</label>
-        <input type="text" id="zip" />
+        <input type="text" id="zip" name="zip" />
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" />
+        <input type="email" id="email" name="email" />
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input type="password" id="password" name="password" />
         <label htmlFor="pets">Pets</label>
-        <select>
+        <select id="pets" name="pets">
           <option value=""></option>
           <option value="yes">yes</option>
           <option value="no">no</option>
         </select>
         <label htmlFor="admin">Admin</label>
-        <input type="checkbox" />
+        <input type="checkbox" id="admin" name="admin" />
         <button>Submit</button>
       </form>
     </div>
